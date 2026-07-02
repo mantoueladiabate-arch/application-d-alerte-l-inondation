135b9ff7af073f43ae5cdb894472666d1a23ba2b
SAMB SIDY <abdoulaye.samb@sahanalytics.com>
2026-07-02 15:55:31 +0000

    config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '5432');
+define('DB_NAME', 'base_inondation');
+define('DB_USER', 'postgres');
+define('DB_PASS', '0151516084');
+define('DB_DSN',  'pgsql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME);
+
+// HSMS.CI - API SMS
+define('SMS_TOKEN',         '0eebac3b6594eb3c37b675f8ab0299629f5d96f9');
+define('SMS_CLIENT_ID',     'OVLSERVICES_GtC9346');
+define('SMS_CLIENT_SECRET', 'OVLSERVICES20251024094434.7668209a8NIMFxWldCgbbmFPCZ');
